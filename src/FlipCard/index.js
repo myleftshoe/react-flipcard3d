405e6680bb99253a6a76e45a5f0cd43b2202df55
Paul <paulesammut@gmail.com>
2019-01-01 18:16:17 +1100

    minor

--- src/FlipCard/index.js
+++ src/FlipCard/index.js
@@ -43,25 +43,25 @@ function FlipCard({ axis = 'auto', duration = 800, direction = 'clockwise', onFl
         { transform: `translateZ(-200px) rotate${_axis}(0deg) scale(${scale})` },
         { transform: `translateZ(-100px) rotate${_axis}(0deg) scale(${scale})`, offset: 0.15 },
         { transform: `translateZ(-100px) rotate${_axis}(${minus}180deg) scale(${scale})`, offset: 0.65 },
-        { transform: `translateZ(-200px) rotate${_axis}(${minus}180deg) scale(${scale})`, offset: 1 }
+        { transform: `translateZ(-200px) rotate${_axis}(${minus}180deg) scale(${scale})` }
       ],
       sideTwo: [
         { transform: `translateZ(-200px) rotate${_axis}(${minus}180deg) scale(${scale})` },
         { transform: `translateZ(-100px) rotate${_axis}(${minus}180deg) scale(${scale})`, offset: 0.15 },
         { transform: `translateZ(-100px) rotate${_axis}(${minus}360deg) scale(${scale})`, offset: 0.65 },
-        { transform: `translateZ(-200px) rotate${_axis}(${minus}360deg) scale(${scale})`, offset: 1 }
+        { transform: `translateZ(-200px) rotate${_axis}(${minus}360deg) scale(${scale})` }
       ],
       umbra: [
         { opacity: 0.3, transform: `translateY(2px) rotate${_axis}(0deg)` },
         { opacity: 0.0, transform: `translateY(62px) rotate${_axis}(0deg)`, offset: 0.15 },
         { opacity: 0.0, transform: `translateY(62px) rotate${_axis}(${minus}180deg)`, offset: 0.65 },
-        { opacity: 0.3, transform: `translateY(2px) rotate${_axis}(${minus}180deg)`, offset: 1 }
+        { opacity: 0.3, transform: `translateY(2px) rotate${_axis}(${minus}180deg)` }
       ],
       penumbra: [
         { opacity: 0.0, transform: `translateY(2px) rotate${_axis}(0deg)` },
         { opacity: 0.5, transform: `translateY(62px) rotate${_axis}(0deg)`, offset: 0.15 },
         { opacity: 0.5, transform: `translateY(62px) rotate${_axis}(${minus}180deg)`, offset: 0.65 },
-        { opacity: 0.0, transform: `translateY(2px) rotate${_axis}(${minus}180deg)`, offset: 1 }
+        { opacity: 0.0, transform: `translateY(2px) rotate${_axis}(${minus}180deg)` }
       ],
     }
 
